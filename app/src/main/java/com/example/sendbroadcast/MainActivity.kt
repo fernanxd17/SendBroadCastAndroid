dff07de0c2dc067a380d5dde2e2f5a0f26e54da0
Juan Fernando Romero Ortega <fernando.romero@ceiba.com.co>
2021-02-23 17:31:46 -0500

    Enviando una emisión a cualquier aplicación interesada.

--- app/src/main/java/com/example/sendbroadcast/MainActivity.kt
+++ app/src/main/java/com/example/sendbroadcast/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.sendbroadcast
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sendbroadcast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = activityMainBinding.root
+        setContentView(view)
+
+        activityMainBinding.button.setOnClickListener {
+            sendBroadcast()
+        }
+    }
+
+    private fun sendBroadcast() {
+        Intent().also { intent ->
+            intent.action = "CUSTOM_NOTIFICATION"
+            intent.putExtra("data", "Enviando un saludo")
+            sendBroadcast(intent)
+        }
     }
 }
